426514a41ceae8f71e220c4cd2436062d2135aca
mengqianliu <mengqianliu@email.arizona.edu>
2016-09-27 02:13:18 -0700

    Restaurant Recommendation
    Restaurant Recommendation WebService.

---
+++ Dashi/src/api/SearchRestaurants.java
@@ -0,0 +1,78 @@
+package api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.MongoDBConnection;
+import db.MySQLDBConnection;
+
+/**
+ * Servlet implementation class SearchRestaurants
+ */
+@WebServlet("/restaurants")
+public class SearchRestaurants extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static DBConnection connection = new MySQLDBConnection();
+	//private static DBConnection connection = new MongoDBConnection();   
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchRestaurants() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// allow access only if session exists
+				HttpSession session = request.getSession();
+				if (session.getAttribute("user") == null) {
+					response.setStatus(403);
+					return;
+				}
+		JSONArray array = new JSONArray();//一组JSon object的集合
+		//try {
+			if (request.getParameterMap().containsKey("user_id")
+					&& request.getParameterMap().containsKey("lat")
+					&& request.getParameterMap().containsKey("lon")) {
+				String userId = request.getParameter("user_id"); // getParameter return String
+				double lat = Double.parseDouble(request.getParameter("lat"));
+				double lon = Double.parseDouble(request.getParameter("lon"));
+				// return some fake restaurants
+				array = connection.searchRestaurants(userId, lat, lon);
+//				array.put(new JSONObject().put("name", "Panda Express"));
+//				array.put(new JSONObject().put("name", "Hong Kong Express"));
+			}
+//		} catch (JSONException e) {
+//			e.printStackTrace();
+//		}
+		RpcParser.writeOutput(response, array);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	// doDelete
+
+}
